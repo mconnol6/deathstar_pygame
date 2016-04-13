2db8be18599890e5cf8475f6c4d93c3e1fc3d629
Mary Connolly <mconnol6@nd.edu>
2016-04-13 15:36:14 -0400

    globe on screen

--- objects.py
+++ objects.py
@@ -14,6 +14,7 @@ class Gamespace(object):
 		black = 0, 0, 0
 
 		self.deathstar = Deathstar(self)
+		self.earth = Earth(self)
 
 		while 1:
 			#click tick
@@ -29,10 +30,34 @@ class Gamespace(object):
 			#black screen
 			self.screen.fill(black)
 
+			#tick updates
+			self.earth.tick()
+
+			#display images
+			self.screen.blit(self.earth.image, self.earth.rect)
+
 			pygame.display.flip()
 
 class Deathstar(pygame.sprite.Sprite):
 
 	#pass in gamespace as argument
-	def __init__(self, gs):
+	def __init__(self, gs=None):
+		pass
+
+class Earth(pygame.sprite.Sprite):
+
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("globe.bmp")
+		self.rect = self.image.get_rect()
+		self.rect.center = 600, 400
+
+		self.orig_image = self.image
+
+		self.exploding = False
+
+	def tick(self):
+
 		pass
